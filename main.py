fbfe84275f65eba3a1ac656bc8d8c2ec5f1bd1c7
Yoel <yobelb8@gmail.com>
2024-10-18 09:49:08 +0100

    Arreglado funcionamiento indice y añadido 40% contenido

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ indice = 0
 data = response.json()
 api_response = 0
 
+# product_list.products[indice].id
 
 tituloL = ""
 categoryL = ""
 ratingL = ""
 precioDiscount = ""
-discountL = ""
 stockL = ""
 descripcionL = ""
 dimencionesL = ""
@@ -33,46 +33,66 @@ skuL = ""
 
 
 def cargar():
-    global indice, tituloL, categoryL, ratingL, precioDiscount, discountL, stockL, descripcionL, dimencionesL, minOrderL, skuL
+    global indice, tituloL, categoryL, ratingL, precioDiscount, stockL, descripcionL, dimencionesL, minOrderL, skuL
     if indice < len(product_list.products):
-        si = 0
+        tituloL.config(text=product_list.products[indice].title)
+        categoryL.config(text=product_list.products[indice].category)
+        ratingL.config(text=f"Rating: {product_list.products[indice].rating}")
+        precioDiscount.config(text=f"{product_list.products[indice].price}, (Discount: {product_list.products[indice].discountPercentage})")
 
 
 def siguienteP():
     global indice
     indice += 1
+    if indice < len(product_list.products):
+        cargar()
+    else:
+        indice = -1
 
 def main():
-    global tituloL, categoryL, ratingL, precioDiscount, discountL, stockL, descripcionL, dimencionesL, minOrderL, skuL
+    global tituloL, categoryL, ratingL, precioDiscount, stockL, descripcionL, dimencionesL, minOrderL, skuL
+    # Pantalla
     root = tk.Tk()
     root.resizable(width=False, height=False)
-    root.geometry("1200x600")
+    root.geometry("900x400")
     root.title("Productos")
-    producto1 = tk.Frame(root)
-    producto1.pack(side="top", fill="x")
+    #Frame 1/2
+    producto1 = tk.Frame(root, height=300)
+    producto1.pack(side="top", fill="x", expand=True)
 
-    img = tk.Frame(producto1, width=40, height=40)
+    #Frame 1.5/2
+    img = tk.Frame(producto1, width=200, height=200)
     img.pack(side="left")
 
-
+    #Frame 2.0/0
     desc = tk.Frame(producto1)
     desc.pack(side="left", fill="x")
 
     tituloL = ttk.Label(producto1, text="1", font=("Sans", 15), justify="left")
-    tituloL.pack(side="top")
+    tituloL.pack(side="top", anchor="w")
     categoryL = ttk.Label(producto1, text="1", font=("Sans", 10), justify="left")
-    categoryL.pack(side="top")
+    categoryL.pack(side="top", anchor="w")
     ratingL = ttk.Label(producto1, text="1", font=("Sans", 10), justify="left")
-    ratingL.pack(side="top")
+    ratingL.pack(side="top", anchor="w")
     precioDiscount = ttk.Label(producto1, text="1", font=("Sans", 10), justify="left")
-    precioDiscount.pack(side="top")
+    precioDiscount.pack(side="top", anchor="w")
 
-
+    #Frame 2/2
     producto2 = tk.Frame(root)
-    producto2.pack(side="top", fill="x", expand=True)
+    producto2.pack(side="top", fill="both", expand=True)
 
+    stockL = ttk.Label(producto2, text="1", font=("Sans", 10), justify="left")
+    stockL.pack(side="top", anchor="w")
+    descripcionL = ttk.Label(producto2, text="1", font=("Sans", 10), justify="left")
+    descripcionL.pack(side="top", anchor="w")
+    dimencionesL = ttk.Label(producto2, text="1", font=("Sans", 10), justify="left")
+    dimencionesL.pack(side="top", anchor="w")
+    minOrderL = ttk.Label(producto2, text="1", font=("Sans", 10), justify="left")
+    minOrderL.pack(side="top", anchor="w")
+    skuL = ttk.Label(producto2, text="1", font=("Sans", 20), justify="left")
+    skuL.pack(side="top", anchor="w")
 
-    siguiente = ttk.Button(text="Siguiente", command=siguienteP(), padding=10)
+    siguiente = ttk.Button(text="Siguiente", command=siguienteP, padding=10)
     siguiente.pack()
 
     cargar()
